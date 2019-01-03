9ec93cdf46384895884c24e44cef76870733c92c
Lars Atzinger <lars.atzinger@aol.de>
2019-01-03 08:39:39 +0100

    init maven project

---
+++ servicebroker/src/test/java/de/thbingen/epro/projekt/ProjektApplicationTests.java
@@ -0,0 +1,17 @@
+package de.thbingen.epro.projekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjektApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
