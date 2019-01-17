32d742bc381370f9fbd24a99f04da727a70816ef
Lars Atzinger <lars.atzinger@aol.de>
2019-01-17 21:36:28 +0100

    added simple inMemory authentication (currently permitAll) and default application.yml configuration

--- servicebroker/src/main/java/de/thbingen/epro/project/config/SecurityConfiguration.java
+++ servicebroker/src/main/java/de/thbingen/epro/project/config/SecurityConfiguration.java
@@ -1,6 +1,16 @@
+/**
+ * Web-Security Configuration.
+ *
+ * @since 1.0
+ * @author larsatzinger
+ * @version 1.0
+ */
+
 package de.thbingen.epro.project.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +18,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //TODO add authentication provider
+        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //TODO add antMatchers and fullyAuthenticated
+        http
+                .authorizeRequests().anyRequest()
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf();
+    }
 }
