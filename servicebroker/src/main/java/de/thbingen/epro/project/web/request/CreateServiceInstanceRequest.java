f28fdbc72bdcf49062c8ee9cdf361ec075a1e366
jhueg <jhueg@local.local>
2019-01-18 12:36:00 +0100

    Initial ServiceInstanceController draft. Added packages for requests and responses. Added CreateServiceInstanceRequest and -Response

---
+++ servicebroker/src/main/java/de/thbingen/epro/project/web/request/CreateServiceInstanceRequest.java
@@ -0,0 +1,44 @@
+package de.thbingen.epro.project.web.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Map;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class CreateServiceInstanceRequest {
+    @NotNull
+    @NotEmpty
+    @NonNull
+    @JsonProperty("service_id")
+    private String serviceId;
+
+    @NotNull
+    @NotEmpty
+    @NonNull
+    @JsonProperty("plan_id")
+    private String planId;
+
+    @NotNull
+    @NotEmpty
+    @NonNull
+    @JsonProperty("organization_guid")
+    private String organizationGuid;
+
+    @NotNull
+    @NotEmpty
+    @NonNull
+    @JsonProperty("space_guid")
+    private String spaceGuid;
+
+
+    private Map<String, String> parameters;
+}
