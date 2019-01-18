8d05ef5d15bfa9c4ef5f01d79bd96207ffa2c75a
jhueg <jhueg@local.local>
2019-01-18 12:49:29 +0100

    Completed CreateServiceInstanceResponse

--- servicebroker/src/main/java/de/thbingen/epro/project/web/response/CreateServiceInstanceResponse.java
+++ servicebroker/src/main/java/de/thbingen/epro/project/web/response/CreateServiceInstanceResponse.java
@@ -1,4 +1,18 @@
 package de.thbingen.epro.project.web.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateServiceInstanceResponse {
+    @NotEmpty
+    private String dashboardUrl;
+
+    @NotEmpty
+    private String operation;
 }
