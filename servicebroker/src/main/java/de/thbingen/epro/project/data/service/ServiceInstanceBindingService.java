8669140d2d9b59b535b6108e60a6e58dd19aa3ed
Lars Atzinger <lars.atzinger@aol.de>
2019-01-24 13:46:49 +0100

    added ServiceInstanceRepository to ServiceInstanceBindingService

--- servicebroker/src/main/java/de/thbingen/epro/project/data/service/ServiceInstanceBindingService.java
+++ servicebroker/src/main/java/de/thbingen/epro/project/data/service/ServiceInstanceBindingService.java
@@ -1,12 +1,16 @@
 package de.thbingen.epro.project.data.service;
 
 import de.thbingen.epro.project.data.repository.ServiceInstanceBindingRepository;
+import de.thbingen.epro.project.data.repository.ServiceInstanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ServiceInstanceBindingService {
 
     @Autowired
     private ServiceInstanceBindingRepository serviceInstanceBindingRepository;
-    
+
+    @Autowired
+    private ServiceInstanceRepository serviceInstanceRepository;
+
 
 }
