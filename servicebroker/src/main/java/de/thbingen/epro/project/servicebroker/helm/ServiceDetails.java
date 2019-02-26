45b9bf230852efb369ec3db7a9b57e384c40c01d
Lars Atzinger <lars.atzinger@aol.de>
2019-02-26 22:13:20 +0100

    added some Methods to ServiceDetails

--- servicebroker/src/main/java/de/thbingen/epro/project/servicebroker/helm/ServiceDetails.java
+++ servicebroker/src/main/java/de/thbingen/epro/project/servicebroker/helm/ServiceDetails.java
@@ -1,13 +1,45 @@
 package de.thbingen.epro.project.servicebroker.helm;
 
 import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServicePort;
 import lombok.Data;
 import lombok.NonNull;
+
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class ServiceDetails {
 
     @NonNull
     private Service service;
+
+    public String getUUID(){
+        return service.getMetadata().getUid();
+    }
+
+    public String getServiceName(){
+        return service.getMetadata().getName();
+    }
+
+    public String getClusterName(){
+        return service.getMetadata().getClusterName();
+    }
+
+    public String getServiceNameSpace(){
+        return service.getMetadata().getNamespace();
+    }
+
+    public Map<String, String> getServiceLabels(){
+        return service.getMetadata().getLabels();
+    }
+
+    public List<ServicePort> getServicePorts(){
+        return service.getSpec().getPorts();
+    }
+
+    public String getServiceType(){
+        return service.getSpec().getType();
+    }
 
 }
--- servicebroker/src/test/java/de/thbingen/epro/project/servicebroker/helm/DefaultKubernetesClientTest.java
+++ servicebroker/src/test/java/de/thbingen/epro/project/servicebroker/helm/DefaultKubernetesClientTest.java
@@ -1,12 +1,9 @@
 package de.thbingen.epro.project.servicebroker.helm;
 
 import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.ServicePort;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-
-import java.util.List;
 
 @Slf4j
 public class DefaultKubernetesClientTest {
@@ -19,10 +16,6 @@ public class DefaultKubernetesClientTest {
 
             Service service = kubernetesClient.services().inNamespace("default").withName("helm-default").get();
 
-            List<ServicePort> servicePorts = service.getSpec().getPorts();
-            servicePorts.stream().map(servicePort -> servicePort.getNodePort()).forEach(System.out::println);
-
-            
 
         }
 
